ad73a1727edb459c4480169c944a72c0fe327061
Arushi <raja@MacAir-AA.local>
2020-09-19 11:05:48 -0700

    Updates

---
+++ precache-manifest.20dd94ffd603315fabb3b14b9ce90efe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341147c3dbdec6bc63d8422b872c1aeb",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "a3d4a4a4677bb87b2104",
+    "url": "/react-deploy/static/css/main.bcce2ac8.chunk.css"
+  },
+  {
+    "revision": "74f8da632b07984961fe",
+    "url": "/react-deploy/static/js/2.7315253c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.7315253c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3d4a4a4677bb87b2104",
+    "url": "/react-deploy/static/js/main.a3e6ed59.chunk.js"
+  },
+  {
+    "revision": "176847e9b27726a77542",
+    "url": "/react-deploy/static/js/runtime-main.63d45127.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
